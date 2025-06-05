0e3fa7cc9b46cd6220bf01c214d38b765e1f9f92
rkovachfuentes <rkovachfuentes@rice.edu>
2025-06-05 16:58:32 -0500

    aclgad_get_area reading files

--- yepes_code/analysis_filepath_correction.py
+++ yepes_code/analysis_filepath_correction.py
@@ -19,7 +19,7 @@ from matplotlib.widgets import SpanSelector
 
 df = pd.read_csv('/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/ACLGAD_only.csv')
 df['Filename min'] = df['Filename min'].str.replace("mar24", "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/2025-03-24/tek")
-df['Filename max'] = df['Filename min'].str.replace("mar24", "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/2025-03-24/tek")
+df['Filename max'] = df['Filename max'].str.replace("mar24", "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/2025-03-24/tek")
 suffix = "CH1.csv"
 df['Filename min'] = df['Filename min'].apply(lambda x: f"{x}{suffix}")
 df['Filename max'] = df['Filename max'].apply(lambda x: f"{x}{suffix}")
