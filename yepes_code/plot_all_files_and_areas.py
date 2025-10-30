0d7445cbb65d9820a627dbde1f71b170d6e19a1f
rkovachfuentes <rkovachfuentes@rice.edu>
2025-10-30 01:46:59 -0500

    added oscillation q feature

--- yepes_code/plot_all_files_and_areas.py
+++ yepes_code/plot_all_files_and_areas.py
@@ -14,26 +14,39 @@ from scipy.optimize import curve_fit
 from scipy.signal import savgol_filter
 # from generate_all_files import plot_area_vs_distance
 
-output_file = "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/all_files_and_areas.csv"
+output_file = "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_06_all_files_and_areas.csv"
 lower_limits = {"0.1":1e-11, "0.5":1e-8,"1.0":1e-8,"2.0":1e-8,"3.0":1e-8}
 
-def percent_diff_removed(original_dir, current_dir):
+def count_file_no_dif(file1, file2):
+    file_path = 'your_file.csv'
+    with open(file1, 'r') as f1:
+        reader1 = csv.reader(f1)
+        row_count1 = len(list(reader1))
+    f1.close()
+    with open(file2, 'r') as f2:
+        reader = csv.reader(f2)
+        row_count2 = len(list(reader1))
+    f2.close()
+    print((row_count_1-row_count_2)/row_count_2)
+    return (row_count_1-row_count_2)/row_count_2
+
+def percent_diff_removed(original_dir, current_dir1, current_dir2):
     original_lst = os.listdir(original_dir)
-    current_lst = os.listdir(current_dir)
-    # len_or = len(original_lst)
-    len_or = 1173
-    len_curr = len(current_lst)
+    current_lst1 = os.listdir(current_dir1)
+    current_lst2 = os.listdir(current_dir2)
+    len_or = len(original_lst)
+    len_curr = len(current_lst1) + len (current_lst2)
     print((len_curr-len_or)/len_or)
     return (len_curr-len_or)/len_or
 
 def filter_small_values(output_file, lower_limits, dropped_file):
     output_file_df = pd.read_csv(output_file)
-    channels = ["CH1","CH2"]
+    channels = ["CH1"]
     for index, row in output_file_df.iterrows():
         pulse = row["Pulse"]
         ifile = int((row["File"].split("-"))[-1].replace(".csv",""))
-        if (float(row["ch1_area"]) < lower_limits[str(pulse)]) or (float(row["ch2_area"]) < lower_limits[str(pulse)]):
-            graphicDir = f"/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-07-30/all_files"
+        if (float(row["ch1_area"]) < lower_limits[str(pulse)]):
+            graphicDir = f"/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-05-06/all_files"
             for channel in channels:
                 graphicFile=f"{graphicDir}/dose-calc-pulse={int(pulse)}-{ifile}-{channel}.jpg"
                 print(graphicFile)
@@ -55,11 +68,13 @@ def cleanup_output_file(output_file):
     output_file_df.to_csv(output_file)
     return
 
-def plot_a_vs_b(log_file, a, b, filter_var, filter_val, group_var, savePlot = False, no_3 = False):
+def plot_a_vs_b(log_file, a, b, a_unit, b_unit, filter_var, filter_val, group_var, savePlot = False, no_3 = False):
     log_df = pd.read_csv(log_file)
-    log_df = log_df[[a, b, filter_var, group_var]]
+    log_df = log_df[["Beam", a, b, filter_var, group_var]]
+    log_df = log_df[log_df["Beam"] == "Electrons 85V"]
     log_df = log_df[log_df[filter_var] == filter_val]
     log_df = log_df.drop(filter_var, axis=1)
+    log_df = log_df.drop("Beam", axis=1)
     print(log_df.head())
     color_dict = {"0.1":"red","0.5":"black","1.0":"blue","2.0":"green","3.0":"yellow"}
     pulse_list = ["0.1","0.5","1","2","3"]
@@ -72,7 +87,6 @@ def plot_a_vs_b(log_file, a, b, filter_var, filter_val, group_var, savePlot = Fa
     for group_tuple, val in grouped_mean_multi:
         initial_grp = group_tuple[0]
         break
-    print(initial_grp)
     grp_means = []
     grp_a = []
     grp_errs = []
@@ -92,11 +106,12 @@ def plot_a_vs_b(log_file, a, b, filter_var, filter_val, group_var, savePlot = Fa
                 initial_grp = group_tuple[0]
                 print(initial_grp)
 
-    ax.set_title(f"{a} vs Mean {b}, Grouped By {group_var}, HV={filter_val}")
+    ax.set_title(f"{a} ({a_unit}) vs Mean {b} ({b_unit}), Grouped By {group_var}, HV={filter_val}")
     ax.set_xlabel(f"{a}")
     ax.set_ylabel(f"{b}")
     ax.legend()
-    plt.show()
+    if savePlot:
+        plt.savefig(f"plot_graveyard/{a}_vs_{b}_grouped_by_{group_var}_{filter_var}={filter_val}.png")
     
     return
 
@@ -124,7 +139,7 @@ def plot_a_vs_b_fixed_Z(log_file, a, b, a_unit, b_unit, Z_value, filter_var, fil
         plt.errorbar(val[a].mean(), val[b].mean(), yerr=val[b].std(), xerr = val[a].std(), color=color_dict[str(group_tuple[0])], label=f"{group_tuple[0]} us", fmt='-o')
         initial_grp = group_tuple[0]
 
-    ax.set_title(f"{a} vs Mean {b}, Grouped By {group_var}, {filter_var}={filter_val} Z=50")
+    ax.set_title(f"{a} vs Mean {b}, Grouped By {group_var}, {filter_var}={filter_val} Z={Z_value}")
     ax.set_xlabel(f"{a}, {a_unit}")
     ax.set_ylabel(f"{b}, {b_unit}")
     ax.legend(loc='upper left')
@@ -155,9 +170,27 @@ def plot_a_vs_b_both_mean(log_file, a, b, a_unit, b_unit, filter_var, filter_val
     plt.show()
     
     return
-# log_file, a, b, a_unit, b_unit, filter_var, filter_val, group_var, savePlot = False, no_3 = False
-plot_a_vs_b_fixed_Z("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/all_files_and_areas_dropped.csv", "Dose", "ch2_area", "C*10^-8", "dimensionless", 50, "HV", 50, "Pulse", savePlot=False, no_3 = True)
 
-# percent_diff_removed("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-07-30/Beam=Electrons 85V-Z=60-HV=0","/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-07-30/all_files")
+# dropped_file = "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_06_all_files_and_areas_dropped.csv"
+# filter_small_values(output_file, lower_limits, dropped_file)
+file_05 = pd.read_csv("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_05_all_files_and_areas_dropped.csv")
+file_06 = pd.read_csv("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_06_all_files_and_areas_dropped.csv")
+# combined_df = pd.concat([file_05, file_06], ignore_index=True)
+# combined_df.to_csv('/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/combined_file.csv', index=False)
+print("may 6")
+# count_file_no_dif("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_06_all_files_and_areas_dropped.csv", "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_06_all_files_and_areas.csv")
+print("may 5")
+# count_file_no_dif("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_05_all_files_and_areas_dropped.csv", "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/05_05_all_files_and_areas.csv")
+HV_list = [10, 20, 30, 40, 50, 60, 80, 100]
+# dist_list = [50, 60, 70]
+# plot_a_vs_b("combined_file.csv", "Z", "Dose", "HV", 60, "Pulse", savePlot=False, no_3 = False)
+'''plots_to_gen = [["Dose", "ch1_area", "C*10^-8", ""], ["Z", "ch1_area", "cm from collimator edge", ""], ["Z", "Dose", "cm from collimator edge", "C*10^-8"]]
+for lst in plots_to_gen:
+    for HV in HV_list:
+        plot_a_vs_b("combined_file.csv", lst[0], lst[1], lst[2], lst[3], "HV", HV, "Pulse", savePlot=True, no_3 = False)'''
+
+plot_a_vs_b("combined_file.csv", "Dose", "ch1_area", "C*10^-8", "", "HV", "HV", "Pulse", savePlot=True, no_3 = False)
+
+# percent_diff_removed("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-07-30/all_files","/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-05-05/all_files","/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/new-plot-dose-2025-05-06/all_files")
 # plot_a_vs_b_both_mean("/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/all_files_and_areas_dropped.csv", "ch1_peaks", "ch2_peaks", "HV", 20, "Pulse", savePlot=False, no_3 = True)
 # filter_small_values(output_file, lower_limits, "/Users/rkfuentes/Documents/md_anderson_analysis/yepes_code/all_files_and_areas_dropped.csv")
